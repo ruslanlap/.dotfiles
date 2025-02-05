2a504dd58dcc975f9bbfadfdd33bb126e21006fe
ruslanlap <lapin@ucu.edu.ua>
2025-02-05 19:50:50 +0200

    new add

--- pre-install.sh
+++ pre-install.sh
@@ -9,14 +9,25 @@ else
   echo 'Updating oh-my-zsh'
 fi
 
+# Install LazyVim
+echo "Setting up LazyVim..."
+# Backup existing Neovim configurations
+mv ~/.config/nvim{,.bak} 2>/dev/null || true
+mv ~/.local/share/nvim{,.bak} 2>/dev/null || true
+mv ~/.local/state/nvim{,.bak} 2>/dev/null || true
+mv ~/.cache/nvim{,.bak} 2>/dev/null || true
 
+# Clone and setup LazyVim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
 
-
+# Install oh-my-zsh plugins and themes
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+
+# Install and setup fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
