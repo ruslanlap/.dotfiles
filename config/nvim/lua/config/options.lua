c4272e0824edfd7ee1bc5fd558fec1bb88b25f99
ruslanlap <lapin@ucu.edu.ua>
2025-02-06 22:09:52 +0200

    new add

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,4 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
@@ -14,18 +13,10 @@ vim.opt.guicursor = {
 -- Enable system clipboard integr
 --
 -- Reset clipboard settings
-vim.opt.clipboard = ''
-
--- Set clipboard provider
-vim.g.clipboard = {
-  name = 'myClip',
-  copy = {
-    ['+'] = 'xsel -ib',
-    ['*'] = 'xsel -ib',
-  },
-  paste = {
-    ['+'] = 'xsel -ob',
-    ['*'] = 'xsel -ob',
-  },
-  cache_enabled = false,
-}
+-- Просте налаштування clipboard
+--
+--
+--
+--
+--
+vim.opt.clipboard = 'unnamedplus'
